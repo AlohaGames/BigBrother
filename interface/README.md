f48adb447e7d302b3dffbf87b81c755a5bdd1d7c
Xenilov <tristan.couturier1998@gmail.com>
2022-01-03 17:42:19 +0100

    :sparkles: Add interface

---
+++ interface/README.md
@@ -0,0 +1,3 @@
+# Interface
+
+NodeJS application that connect to Arduino Serial Port to receive data and display them on a webpage.
---
+++ interface/index.js
@@ -0,0 +1,44 @@
+const Readline = require("@serialport/parser-readline");
+const SerialPort = require("serialport");
+
+async function main() {
+  // Get list of ports
+  const ports = await SerialPort.list();
+
+  // Filter only Arduino ones
+  const arduinoPorts = ports.filter(
+    (p) => p.manufacturer && p.manufacturer.includes("Arduino")
+  );
+
+  if (arduinoPorts.length <= 0) {
+    throw new Error("No Arduino ports found !");
+  }
+
+  // Init Serial
+  const arduinoPort = arduinoPorts[0];
+  console.log(`Using ${arduinoPort.manufacturer} on ${arduinoPort.path}`);
+  const serialport = new SerialPort(arduinoPort.path, {
+    baudRate: 9600,
+  });
+
+  serialport.on("open", () => {
+    console.log("Connection with Arduino open !");
+  });
+
+  serialport.on("close", () => {
+    console.log("Connection with Arduino closed !");
+  });
+
+  serialport.on("error", (err) => {
+    throw err;
+  });
+
+  // Use ReadLine parser to parse line by line using \n as delimiter
+  const parser = serialport.pipe(new Readline());
+
+  parser.on("data", (data) => {
+    console.log(data);
+  });
+}
+
+main();
