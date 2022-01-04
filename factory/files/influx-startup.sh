7d5c013da70e4550d67533355f90270dfb9001fc
Xenilov <tristan.couturier1998@gmail.com>
2022-01-04 18:05:04 +0100

    :sparkles: Update factory

--- factory/files/influx-startup.sh
+++ factory/files/influx-startup.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
 # Commands to set up influxdb and create 
 # A primary bucket to receive or IoT data
 # A bucket to receive telegraf data
+
+# If it does not work due to bad interpreter :
+# sed -i -e 's/\r$//'
 
 influx setup -u tristan -p aBcJ45f5 -o aloha -b big-brother -r 0 -f
 
