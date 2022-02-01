e0ea22eec64e432aa8cf8b5319d56c1c0ec1ea52
Kévin Plestan <kplestan@enssat.fr>
2022-02-01 17:47:24 +0100

    Adding Bluetooth class v1

---
+++ node/Node1/PinCfg.h
@@ -0,0 +1,12 @@
+//
+// Created by AlohaGames on 01/02/2022.
+//
+
+#ifndef TESTBIGBROTHER_PINCFG_H
+#define TESTBIGBROTHER_PINCFG_H
+
+#define BT_KEY_PIN 4
+#define BT_RX_PIN 3
+#define BT_TX_PIN 2
+
+#endif //TESTBIGBROTHER_PINCFG_H
