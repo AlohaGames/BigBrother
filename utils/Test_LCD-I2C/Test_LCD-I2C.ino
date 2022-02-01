66b532606a80a80bf8713bd49c6e73653017e1e0
Enoriz <enora.morizur@gmail.com>
2022-02-01 14:26:21 +0100

    add lcd screen

--- utils/Test_LCD-I2C/Test_LCD-I2C.ino
+++ utils/Test_LCD-I2C/Test_LCD-I2C.ino
@@ -3,13 +3,26 @@
 LiquidCrystal_I2C lcd(0x27,16,2);
 
 void setup() {
+  Serial.begin(9600);
   lcd.init();
   lcd.backlight();
 }
 
 void loop() {
   lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Hello World !");
+  
+
+  int randomNum = random(0, 2);
+  Serial.println(randomNum);
+
+  if(randomNum == 0){
+    lcd.setCursor(2,0);
+    lcd.print("Acces refuse");
+  }
+  if(randomNum == 1){
+    lcd.setCursor(1,0);
+    lcd.print("Acces autorise");
+  }
+  
   delay(1000);
 }
