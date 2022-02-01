794d47b0a140f2594c5ef739c4e5fb0f272958a5
Kévin Plestan <kplestan@enssat.fr>
2022-02-01 19:29:26 +0100

    :construction: WIP, mais c est pas mal...

--- node/Node1/BluetoothMgr.cpp
+++ node/Node1/BluetoothMgr.cpp
@@ -2,7 +2,6 @@
 // Created by AlohaGames on 01/02/2022.
 //
 #include <Arduino.h>
-#include <String.h>
 
 #include "BluetoothMgr.h"
 #include "PinCfg.h"
@@ -11,39 +10,45 @@ BluetoothMgr::BluetoothMgr() : SoftwareSerial(BT_RX_PIN, BT_TX_PIN) {}
 
 void BluetoothMgr::init()
 {
-    Serial.print("Coucou");
+    pinMode(BT_KEY_PIN, OUTPUT);
+    pinMode(BT_TX_PIN, OUTPUT);
+    pinMode(BT_RX_PIN, INPUT);
+
     this->begin(38400);
 
     delay(1000);
-
-    Serial.print("HeHo");
 
     digitalWrite(BT_KEY_PIN, HIGH);
 
     this->setTimeout(1000);
 
     sendCommand("AT+RESET");
-    log(this->readString());
-
+    logError(this->readString());
 
     sendCommand("AT+ROLE=1");
 
-    log(this->readString());
+    logError(this->readString());
 
-    sendCommand("AT+CMODE=1");
+    sendCommand("AT+CMODE=0");
 
-    log(this->readString());
+    logError(this->readString());
+
+    sendCommand("AT+BIND=98D3,32,10FE28");
+
+    logError(this->readString());
 
     sendCommand("AT+INIT");
 
-    log(this->readString());
+    logError(this->readString());
 
     sendCommand("AT+STATE");
 
-    log(this->readString());
+    logError(this->readString());
 
 
-    sendCommand("AT+LINK=98D3,32,10FE28");
+    /*sendCommand("AT+LINK=98D3,32,10FE28");
+
+    logError(this->readString());*/
 
     digitalWrite(BT_KEY_PIN, LOW);
 }
@@ -51,7 +56,7 @@ void BluetoothMgr::init()
 void BluetoothMgr::sendCommand(const String &command)
 {
     String tmp = command + "\r\n";
-    log("Send command: " + tmp);
+    log("Send command: " + command);
     this->write(tmp.c_str());
 }
 
@@ -63,7 +68,7 @@ void BluetoothMgr::log(const String &msg)
 
 void BluetoothMgr::logError(const String &msg)
 {
-    if(msg != "OK\r\n")
+    if(msg != "OK\r\n" && msg != "")
         Serial.println("BT ERROR: {" + msg + "}");
     else
         Serial.println(msg);
